33101dfbe6261a2110e41a040f61c73c0bd708b5
Shreyash065232 <shreyashdaundkar549@gmail.com>
2026-01-11 18:20:20 +0530

    requirement added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages ,setup
+
+setup(
+    name="medical_chatbot",
+    versison="0.1.0",
+    author="shreyash daundkar",
+    author_email="shreyashdaundkar549@gamil.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
