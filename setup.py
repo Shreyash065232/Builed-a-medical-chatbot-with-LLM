ec6a371c4e346acb86a806a9eeaa90753d48e81e
Shreyash065232 <shreyashdaundkar549@gmail.com>
2026-01-26 12:23:05 +0530

    web app complated

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import find_packages ,setup
+from setuptools import find_packages, setup
 
 setup(
     name="medical_chatbot",
-    versison="0.1.0",
-    author="shreyash daundkar",
-    author_email="shreyashdaundkar549@gamil.com",
+    version="0.1.0",
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
     packages=find_packages(),
     install_requires=[]
-    )
+)
